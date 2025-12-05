e5c84176b4851169882d8eae05a134b054c6fe9d
Lukas Sinkus <shwarus@gmail.com>
2025-12-05 14:16:32 +0100

    add GetWorkerViewEquipment

---
+++ RedYellowGreen/src/RedYellowGreen.Api/Features/Equipment/BaseEquipmentController.cs
@@ -0,0 +1,9 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace RedYellowGreen.Api.Features.Equipment;
+
+[ApiController]
+[Route("api/equipment")]
+public abstract class BaseEquipmentController
+{
+}
---
+++ RedYellowGreen/src/RedYellowGreen.Api/Features/Equipment/Endpoints/GetWorkerViewEquipment.cs
@@ -0,0 +1,53 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RedYellowGreen.Api.Features.Equipment.Models;
+using RedYellowGreen.Api.Infrastructure.Database;
+
+namespace RedYellowGreen.Api.Features.Equipment.Endpoints;
+
+public class GetWorkerViewEquipment : BaseEquipmentController
+{
+    public record Order(Guid Id, string Number, DateTime CreatedAt);
+
+    public record Result(
+        Guid Id,
+        string Title,
+        EquipmentState State,
+        Order? CurrentOrder,
+        Order[] ScheduledOrders
+    );
+
+    [HttpGet("worker-view")]
+    public async Task<Result[]> Handle(
+        [FromServices] AppDbContext context
+    )
+    {
+        var equipment = await context.Equipment
+            .Include(equipment => equipment.Orders)
+            .ToArrayAsync();
+
+        return equipment
+            .Select(e =>
+            {
+                var currentOrder = e.Orders
+                    .OrderBy(o => o.CreatedAt)
+                    .Select(o => new Order(o.Id, o.OrderNumber, o.CreatedAt))
+                    .FirstOrDefault();
+
+                var scheduledOrders = e.Orders
+                    .Where(o => currentOrder is not null && o.Id != currentOrder.Id)
+                    .OrderBy(o => o.CreatedAt)
+                    .Select(o => new Order(o.Id, o.OrderNumber, o.CreatedAt))
+                    .ToArray();
+
+                return new Result(
+                    e.Id,
+                    e.Title,
+                    e.CurrentState.State,
+                    currentOrder,
+                    scheduledOrders
+                );
+            })
+            .ToArray();
+    }
+}
