a6606f1785be1616eaaefafddc150226ea217bca
Lukas Sinkus <shwarus@gmail.com>
2025-12-08 16:47:11 +0100

    init UI

---
+++ UI/src/api/client.ts
@@ -0,0 +1,11 @@
+// src/api/client.ts
+import axios from "axios";
+
+export const baseUrl = import.meta.env.VITE_API_BASE_URL;
+
+export const api = axios.create({
+  baseURL: baseUrl,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
